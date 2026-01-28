67be8a2a9d68a8edfd3574703a964c4f4840c7fb
Petchim <petchim124@gmail.com>
2026-01-29 01:27:24 +0530

    crossorgin updated

--- src/main/java/com/pasteBin/Pastebin/config/CorsConfig.java
+++ src/main/java/com/pasteBin/Pastebin/config/CorsConfig.java
@@ -1,4 +1,5 @@
 package com.pasteBin.Pastebin.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -12,12 +13,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://paste-bin-zeta-five.vercel.app/")
-//                        .allowedOrigins("https://as2wice-o7jv.vercel.app/")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedOrigins("https://paste-bin-zeta-five.vercel.app") // remove trailing slash
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true); // allow cookies/auth if needed
             }
         };
     }
 }
-
